80a81e34bb4fb3ccdb15d86b3911b977b8e308d6
Hyunwoo Kim <khw1031@gmail.com>
2018-04-25 04:38:34 +0900

    Build 1.2.1

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,4 +4,35 @@ export default function withUtterances<P extends {
     location: {
         pathname: string;
     };
-}>(WrappedComponent: React.ComponentClass<P>, repo: string, issueTerm?: string): any;
+}>(WrappedComponent: React.ComponentClass<P>, repo: string): {
+    new (props: P, context?: any): {
+        withUtterancesContainer: React.RefObject<HTMLDivElement>;
+        componentDidMount(): void;
+        injectScript: () => void;
+        injectPrefetch: () => void;
+        injectPreload: () => void;
+        initUtterances: () => void;
+        render(): JSX.Element;
+        setState<K extends never>(state: {} | ((prevState: Readonly<{}>, props: P) => {} | Pick<{}, K>) | Pick<{}, K>, callback?: () => void): void;
+        forceUpdate(callBack?: () => void): void;
+        props: Readonly<{
+            children?: React.ReactNode;
+        }> & Readonly<P>;
+        state: Readonly<{}>;
+        context: any;
+        refs: {
+            [key: string]: React.ReactInstance;
+        };
+        shouldComponentUpdate?(nextProps: Readonly<P>, nextState: Readonly<{}>, nextContext: any): boolean;
+        componentWillUnmount?(): void;
+        componentDidCatch?(error: Error, errorInfo: React.ErrorInfo): void;
+        getSnapshotBeforeUpdate?(prevProps: Readonly<P>, prevState: Readonly<{}>): any;
+        componentDidUpdate?(prevProps: Readonly<P>, prevState: Readonly<{}>, snapshot?: any): void;
+        componentWillMount?(): void;
+        UNSAFE_componentWillMount?(): void;
+        componentWillReceiveProps?(nextProps: Readonly<P>, nextContext: any): void;
+        UNSAFE_componentWillReceiveProps?(nextProps: Readonly<P>, nextContext: any): void;
+        componentWillUpdate?(nextProps: Readonly<P>, nextState: Readonly<{}>, nextContext: any): void;
+        UNSAFE_componentWillUpdate?(nextProps: Readonly<P>, nextState: Readonly<{}>, nextContext: any): void;
+    };
+};
