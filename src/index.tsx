24621f13b49a00fcb9bbd3da8d78230f3570dc79
Hyunwoo Kim <khw1031@gmail.com>
2018-04-25 04:39:13 +0900

    Refactors and prefetch preload Applied 🚀

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,47 @@ import * as React from 'react'
 
 export default function withUtterances<P extends { location: { pathname: string } }>(
   WrappedComponent: React.ComponentClass<P>,
-  repo: string,
-  issueTerm?: string
-): any {
+  repo: string
+) {
   return class extends React.Component<P> {
-    wrappedComponent: React.RefObject<HTMLDivElement> = React.createRef()
+    withUtterancesContainer: React.RefObject<HTMLDivElement> = React.createRef()
 
     componentDidMount() {
+      this.initUtterances()
+    }
+
+    injectScript = () => {
       const script = document.createElement('script')
       script.src = 'https://utteranc.es/client.js'
       script.async = true
       script.setAttribute('repo', repo)
-      const issue = issueTerm || document.location.pathname
-      script.setAttribute('issue-term', issue)
-      this.wrappedComponent.current.appendChild(script)
+      script.setAttribute('issue-term', this.props.location.pathname || document.location.pathname)
+      this.withUtterancesContainer.current.appendChild(script)
+    }
+
+    injectPrefetch = () => {
+      document.head.insertAdjacentHTML(
+        'beforeend',
+        '<link rel="prefetch" href="https://utteranc.es/client.js" />'
+      )
+    }
+
+    injectPreload = () => {
+      document.head.insertAdjacentHTML(
+        'beforeend',
+        '<link rel="preload" href="https://utteranc.es/client.js" as="script" />'
+      )
+    }
+
+    initUtterances = () => {
+      this.injectPrefetch()
+      this.injectPreload()
+      this.injectScript()
     }
 
     render() {
       return (
-        <div ref={this.wrappedComponent}>
+        <div ref={this.withUtterancesContainer}>
           <WrappedComponent {...this.props} />
         </div>
       )
