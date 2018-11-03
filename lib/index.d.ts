6c9aa95a34a1b16a14020a4b019e8c8d285e5569
Hyunwoo Kim <khw1031@gmail.com>
2018-11-03 15:50:34 +0900

    [Update]

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,39 @@
-/// <reference types="react" />
 import * as React from 'react';
 export default function withUtterances<P extends {
     location: {
         pathname: string;
     };
-}>(WrappedComponent: React.ComponentClass<P>, repo: string): {
+}>(WrappedComponent: React.ComponentClass<P>, repo: string, theme?: string): {
+    new (props: Readonly<P>): {
+        withUtterancesContainer: React.RefObject<HTMLDivElement>;
+        componentDidMount(): void;
+        injectScript: () => void;
+        injectPrefetch: () => void;
+        injectPreload: () => void;
+        initUtterances: () => void;
+        render(): JSX.Element;
+        setState<K extends never>(state: {} | ((prevState: Readonly<{}>, props: Readonly<P>) => {} | Pick<{}, K>) | Pick<{}, K>, callback?: () => void): void;
+        forceUpdate(callBack?: () => void): void;
+        readonly props: Readonly<{
+            children?: React.ReactNode;
+        }> & Readonly<P>;
+        state: Readonly<{}>;
+        context: any;
+        refs: {
+            [key: string]: React.ReactInstance;
+        };
+        shouldComponentUpdate?(nextProps: Readonly<P>, nextState: Readonly<{}>, nextContext: any): boolean;
+        componentWillUnmount?(): void;
+        componentDidCatch?(error: Error, errorInfo: React.ErrorInfo): void;
+        getSnapshotBeforeUpdate?(prevProps: Readonly<P>, prevState: Readonly<{}>): any;
+        componentDidUpdate?(prevProps: Readonly<P>, prevState: Readonly<{}>, snapshot?: any): void;
+        componentWillMount?(): void;
+        UNSAFE_componentWillMount?(): void;
+        componentWillReceiveProps?(nextProps: Readonly<P>, nextContext: any): void;
+        UNSAFE_componentWillReceiveProps?(nextProps: Readonly<P>, nextContext: any): void;
+        componentWillUpdate?(nextProps: Readonly<P>, nextState: Readonly<{}>, nextContext: any): void;
+        UNSAFE_componentWillUpdate?(nextProps: Readonly<P>, nextState: Readonly<{}>, nextContext: any): void;
+    };
     new (props: P, context?: any): {
         withUtterancesContainer: React.RefObject<HTMLDivElement>;
         componentDidMount(): void;
@@ -13,9 +42,9 @@ export default function withUtterances<P extends {
         injectPreload: () => void;
         initUtterances: () => void;
         render(): JSX.Element;
-        setState<K extends never>(state: {} | ((prevState: Readonly<{}>, props: P) => {} | Pick<{}, K>) | Pick<{}, K>, callback?: () => void): void;
+        setState<K extends never>(state: {} | ((prevState: Readonly<{}>, props: Readonly<P>) => {} | Pick<{}, K>) | Pick<{}, K>, callback?: () => void): void;
         forceUpdate(callBack?: () => void): void;
-        props: Readonly<{
+        readonly props: Readonly<{
             children?: React.ReactNode;
         }> & Readonly<P>;
         state: Readonly<{}>;
